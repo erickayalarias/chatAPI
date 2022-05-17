627729c4a6e5b5532dd7fbdc10fa694631842d69
Erick Ayala <erickayalrias@gmail.com>
2022-05-17 16:31:14 +0200

    commit

--- config.js
+++ config.js
@@ -2,6 +2,6 @@ const dotenv = require('dotenv');
 dotenv.config();
 
 module.exports = {
-    socket_origin: process.env.SOCKET_ORIGIN,
-    db: process.env.DB,
+    socket_origin: process.env.SOCKET_ORIGIN || 'http://localhost:3000',
+    db: process.env.DB || 3001,
 }
