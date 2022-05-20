b7deb5faafbe06daf6fee9defd9eed0c28cb65e9
Erick Ayala <erickayalrias@gmail.com>
2022-05-20 10:07:32 +0200

    commit

--- config.js
+++ config.js
@@ -2,6 +2,6 @@ const dotenv = require('dotenv');
 dotenv.config();
 
 module.exports = {
-    socket_origin: process.env.SOCKET_ORIGIN || 'https://cabal-erickayalarias.vercel.app/',
+    socket_origin: process.env.SOCKET_ORIGIN || 'https://cabal-erickayalarias.vercel.app',
     db: process.env.DB || 3001,
 }
