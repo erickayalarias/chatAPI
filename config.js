a0823850e8ebf4492ff71a37c3a7021b1f0db6af
Erick Ayala <erickayalrias@gmail.com>
2022-05-20 13:29:18 +0200

    cabal online

--- config.js
+++ config.js
@@ -2,6 +2,6 @@ const dotenv = require('dotenv');
 dotenv.config();
 
 module.exports = {
-    socket_origin: ["cabal-two.vercel.app"],
+    socket_origin: ["cabal-i4nf08rsu-erickayalarias.vercel.app"],
     db: process.env.DB || 3001,
 }
